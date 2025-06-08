a2a05111f8ab082e285ddfe5cef0b4c694e55448
Luan1611 <luan_1611@hotmail.com>
2025-06-08 13:38:48 -0300

    feat: links adicionados no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
+import { Link } from 'react-router-dom'
 import '../styles/Footer.css'
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      {/* <div id='footer-backgound'>
-        <img src={footerBackground} alt='' />
-      </div> */}
       <div className='content'>
         <div className='main-content'>
           <div className='flex-col'>
@@ -13,18 +11,19 @@ const Footer = () => {
             <span>plantstore@gmail.com</span>
             <span>+55 (41) 99999-9999</span>
           </div>
+
           <div className='links'>
             <div className='flex-col'>
               <h3>Links</h3>
-              <span>About Us</span>
-              <span>Products</span>
-              <span>Blogs</span>
+              <Link to="/about-us">About Us</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/quiz">Quiz</Link>
             </div>
             <div className='flex-col'>
               <h3>Community</h3>
-              <span>About Us</span>
-              <span>Products</span>
-              <span>Blogs</span>
+              <Link to="/about-us">About Us</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/quiz">Quiz</Link>
             </div>
           </div>
         </div>
